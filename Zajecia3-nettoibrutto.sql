215e5f0bde53eb7467cd7d5fa8aed94a52991724
Natalia Krawczyk <116748236+NataliaKrawczyk03@users.noreply.github.com>
2025-07-18 09:17:08 +0200

    Delete Zajecia3-nettoibrutto.sql

--- Zajecia3-nettoibrutto.sql
+++
@@ -1,7 +0,0 @@
-EXEC sp_rename 'rozliczenia.pensje.kwota', 'kwota_brutto', 'COLUMN';
-
-ALTER TABLE rozliczenia.pensje
-ADD kwota_netto decimal(10,2);
-
-UPDATE rozliczenia.pensje
-SET kwota_netto = ROUND(kwota_brutto * 0.75, 2);
